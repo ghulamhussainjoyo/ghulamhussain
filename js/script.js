9be2ce03c6a53179f4117d98874e0764687ced81
GhulamHussainJoyo <ghulamhussain.kcsf18@iba-suk.edu.pk>
2022-09-23 14:58:42 +0500

    portfolio

---
+++ js/script.js
@@ -0,0 +1,31 @@
+
+window.addEventListener("DOMContentLoaded", function() {
+    
+    var wrap = document.getElementById('wrap');
+    var fps = new FullPageScroll(wrap);
+    console.log(fps)
+    var indicator = document.createElement('div');
+    indicator.id = 'indicator';
+    var slideIndicators = [];
+    fps.slides.forEach(function(slide, index){
+      var slideIndicator = document.createElement('div');
+      slideIndicator.onclick = function() {
+        fps.goToSlide(index);
+      }
+      if (index === fps.currentSlide) {
+        slideIndicator.className = "active";
+      }
+      indicator.appendChild(slideIndicator);
+      slideIndicators.push(slideIndicator);
+    });
+    document.body.appendChild(indicator);
+    fps.onslide = function() {
+      slideIndicators.forEach(function(slideIndicator, index) {
+        if (index === fps.currentSlide) {
+          slideIndicator.className = "active";
+        } else {
+          slideIndicator.className = "";
+        }
+      });
+    }
+  });
